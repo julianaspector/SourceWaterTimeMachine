130358dc0c35e50cd397519aaff4e3a451d5f81d
julianaspector <julianaspector@gmail.com>
2019-07-08 15:32:56 -0700

    Add files via upload
    Includes water source availability and facility type in source point popup

--- server.R
+++ server.R
@@ -11,7 +11,7 @@ shinyServer(
     areas <- subset(serviceAreas, d_prin_cnt == input$counties)
     points <- subset(sourcePoints, PWS.ID == input$pwsid)
     map  <- addPolygons(map, data=areas, popup=areas$pwsid)
-    map  <- addMarkers(map,data=points, lng=~Longitude, lat=~Latitude, popup=points$WSF.Name)
+    map  <- addMarkers(map,data=points, lng=~Longitude, lat=~Latitude, popup=paste("Name:",points$WSF.Name,"<br>", "Source Availability:", points$Availability, "<br>", "Water Source Facility Type:", points$WSF.Type))
     map
     }
   )
