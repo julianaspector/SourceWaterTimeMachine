89d3b7c183507ded14b69f03de409f8786d964f7
Juliana Spector <julianaspector@gmail.com>
2019-09-24 15:21:57 -0700

    Added sidebar panel with app user instructions

--- ui.R
+++ ui.R
@@ -2,7 +2,39 @@
 
 ui<-fluidPage(
   useShinyjs(),
-  titlePanel("California Public Systems Source Water Locations and Information"),
+  titlePanel("California Public Water Systems Source Locations and Information"),
+  sidebarPanel("The State Water Board's Division of Drinking Water (DDW) regulates approximately 7,500 public water systems (PWS) in California. A PWS is defined as a system that provides drinking water for human consumption to 15 or more connections or regularly serves 25 or more people daily for at least 60 days out of the year. The DDW allocates permits for PWS, and collects an annual report from each system.",
+              br(),
+              br(),
+              tags$b("How to use this app:"), 
+              br(),
+              "1) Select a county of interest to zoom map into relevant area.",
+              br(),
+              "2) Click on public water system area to see Public Water System ID.",
+              br(),
+              "3) Select a Public Water System ID of interest.",
+              br(),
+              "4) Click on pins to see name of water source(s), water source availability, and water source facility type. The same information is available in tabular format in the Table tab.",
+              br(),
+              "5) The Graphs tab shows how the water source composition (relative fractions of groundwater, surface water, and delivered water) changed between 2013-2016.",
+              br(),
+              br(),
+              fluidRow(
+                 column(6, offset=1, tableOutput('tbl_facilities')),
+                 column(6, offset=1, tableOutput('tbl_availability'))
+               ),
+               br(),
+               tags$b("Data Sources:"),
+               br(),
+               br(),
+               "Tracking California, Public Health Institute. Water Boundary Tool. Accessed 7/9/2019 from https://www.trackingcalifornia.water",
+               br(),
+               br(),
+               "Division of Drinking Water Source Points, State Water Resources Control Board. California Integrated Water Quality System Project. Accessed 7/2/2019 from https://github.com/CAWaterBoardDataCenter/PWStoSources",
+               br(),
+               br(),
+               "Electronic Annual Report, Safe Drinking Water Information System. Accessed 7/2/2019 from https://data.ca.gov/dataset/drinking-water-public-water-system-annually-reported-water-production-and-delivery-information"
+  ),
   mainPanel(
     tabsetPanel(type="tabs",
                 tabPanel("Map", 
@@ -23,11 +55,9 @@ ui<-fluidPage(
                          ),
                          # add a break beneath button
                          br(),
-                         # make keys and map appear in same row
                          fluidRow(
-                           column(10, leafletOutput("map")),
-                           column(1, offset=1, tableOutput('tbl_facilities')),
-                           column(1, offset=1, tableOutput('tbl_availability'))
+                           column(width=12,
+                                  leafletOutput("map"))
                          )
                 ),
                 tabPanel("Table", tableOutput("table_select")),
