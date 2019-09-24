b7ba43296ed56d678b277180614499770be0e4a1
Juliana Spector <julianaspector@gmail.com>
2019-09-24 10:38:50 -0700

    Delete process.R

--- process.R
+++
@@ -1,7 +0,0 @@
-df <- read.csv("C:/Users/JSpector/Documents/Source Water Time Machine/20190619 DDW Source Points.csv")
-df <- na.omit(df)
-
-saveRDS(df, "C:/Users/JSpector/Documents/Source Water Time Machine/data.rds")
-
-
-
